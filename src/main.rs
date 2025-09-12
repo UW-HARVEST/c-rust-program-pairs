33933952ceea85c66bf6aa3cbac883d2c159d395
wuihee <wuihee@gmail.com>
2025-09-12 11:21:35 -0700

    update documentation formatting

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,26 @@
 //! # C-Rust program pair downloader
 //!
-/// Entry point for the metadata downloader.
-///
-/// # Usage
-///
-/// Download full metadata corpus:
-///
-/// ```sh
-/// cargo run
-/// ```
-///
-/// Download demo/sample metadata only:
-///
-/// ```sh
-/// cargo run demo
-/// ```
-///
-/// Delete program-pairs and repository clones:
-///
-/// ```sh
-/// cargo run delete
-/// ```
+//! Entry point for the metadata downloader.
+//!
+//! # Usage
+//!
+//! Download full metadata corpus:
+//!
+//! ```sh
+//! cargo run
+//! ```
+//!
+//! Download demo/sample metadata only:
+//!
+//! ```sh
+//! cargo run demo
+//! ```
+//!
+//! Delete program pairs and repository clones:
+//!
+//! ```sh
+//! cargo run clear
+//! ```
 use c_rust_program_pairs;
 
 fn main() {
