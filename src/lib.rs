ae6983dab531283876b1a4a02a14ff4292163273
Wuihee <wuihee@Wuihees-MacBook-Air-2.local>
2025-11-03 13:20:21 -0800

    fix(cli): parse cli using clap

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 mod corpus;
 mod paths;
 
-use std::{env, fs, io::Error, path::Path};
+use std::{fs, io::Error, path::Path};
 
 use clap::{Parser, Subcommand};
 
@@ -11,6 +11,7 @@ pub use corpus::download_program_pairs;
 
 use crate::paths::{PROGRAM_PAIRS_DIRECTORY, REPOSITORY_CLONES_DIRECTORY};
 
+/// This struct represents the top-level CLI entry point for the tool.
 #[derive(Parser)]
 #[command(about = "Manages the corpus of C-Rust program pairs", long_about = None)]
 struct Cli {
@@ -18,13 +19,14 @@ struct Cli {
     command: Option<Commands>,
 }
 
+/// This struct represents the different commands available.
 #[derive(Subcommand)]
 enum Commands {
-    /// Downloads all C-Rust program pairs.
-    Download,
-
     /// Downloads a subset of the corpus; used for demonstration.
     Demo,
+
+    /// Downloads all C-Rust program pairs.
+    Download,
 
     /// Delete the `program_pairs` and `repository_clones` directories.
     Delete,
@@ -36,13 +38,14 @@ enum Commands {
 /// all program pairs. If argument "demo" is given, download program pairs
 /// specified within the `demo/` directory.
 pub fn run() {
-    let args: Vec<String> = env::args().collect();
-
-    match args.get(1).map(|arg| arg.as_str()) {
+    let cli = Cli::parse();
+    match cli.command {
         None => download_program_pairs(false).expect("Failed to download program pairs"),
-        Some("demo") => download_program_pairs(true).expect("Failed to run demo"),
-        Some("delete") => delete().expect("Failed to delete directories"),
-        Some(arg) => eprintln!("Invalid argument: {arg}"),
+        Some(Commands::Demo) => download_program_pairs(true).expect("Failed to run demo"),
+        Some(Commands::Download) => {
+            download_program_pairs(false).expect("Failed to download program pairs")
+        }
+        Some(Commands::Delete) => delete().expect("Failed to delete directories"),
     }
 }
 
