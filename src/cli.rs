fc2d0057b4665d801ceec42822db25b2d63df3e2
Wuihee <wuihee@gmail.com>
2025-12-28 00:54:29 -0800

    Use `clap` crate to parse command line arguments

---
+++ src/cli.rs
@@ -0,0 +1,27 @@
+//! # CLI
+//!
+//! This module defines the data structures used to parse command line
+//! arguments when running the program.
+
+use clap::{Parser, Subcommand};
+
+/// This struct represents the top-level CLI entry point for the tool.
+#[derive(Parser)]
+#[command(about = "Manages the corpus of C-Rust program pairs", long_about = None)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Option<Commands>,
+}
+
+/// This struct represents the different commands available.
+#[derive(Subcommand)]
+pub enum Commands {
+    /// Downloads a subset of the corpus; used for demonstration.
+    Demo,
+
+    /// Downloads all C-Rust program pairs.
+    Download,
+
+    /// Delete the `program_pairs` and `repository_clones` directories.
+    Delete,
+}
